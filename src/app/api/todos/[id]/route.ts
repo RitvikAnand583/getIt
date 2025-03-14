e6e2f459a048ca7be6700866d6a5305d9001a666
Ritvik Anand <128292481+RitvikAnand583@users.noreply.github.com>
2025-03-15 04:10:34 +0530

    typo error3 id

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -6,81 +6,81 @@ import prisma from "@/lib/prisma";
 
 export async function PUT(
   req: NextRequest,
-  content: { params: { id: string } }
+  
 ) {
-  const paramsId = content.params.id
-
   const { userId } = await auth();
 
   if (!userId) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      const { completed } = await req.json();
+      console.log(completed);
+      
   }
 
-  try {
-    const { completed } = await req.json();
-    const todoId = paramsId;
+  // try {
+  //   const todoId = params.id;
 
-    const todo = await prisma.todo.findUnique({
-      where: { id: todoId },
-    });
+  //   const todo = await prisma.todo.findUnique({
+  //     where: { id: todoId },
+  //   });
 
-    if (!todo) {
-      return NextResponse.json({ error: "Todo not found" }, { status: 404 });
-    }
+  //   if (!todo) {
+  //     return NextResponse.json({ error: "Todo not found" }, { status: 404 });
+  //   }
 
-    if (todo.userId !== userId) {
-      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
-    }
+  //   if (todo.userId !== userId) {
+  //     return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+  //   }
 
-    const updatedTodo = await prisma.todo.update({
-      where: { id: todoId },
-      data: { completed },
-    });
+  //   const updatedTodo = await prisma.todo.update({
+  //     where: { id: todoId },
+  //     data: { completed },
+  //   });
 
-    return NextResponse.json(updatedTodo);
-  } catch (error) {
-    return NextResponse.json(
-      { error: `Internal Server Error,${error}` },
-      { status: 500 }
-    );
-  }
+  //   return NextResponse.json(updatedTodo);
+  // } catch (error) {
+  //   return NextResponse.json(
+  //     { error: `Internal Server Error,${error}` },
+  //     { status: 500 }
+  //   );
+  // }
 }
 
 export async function DELETE(
-  req: NextRequest,
-  content: { params: { id: string } }
+  req: NextRequest
 ) {
-  const paramsId = content.params.id
   const { userId } = await auth();
 
   if (!userId) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    console.log(req);
+    
   }
 
-  try {
-    const todoId = paramsId;
+  // try {
+  //   const todoId = params.id;
 
-    const todo = await prisma.todo.findUnique({
-      where: { id: todoId },
-    });
+  //   const todo = await prisma.todo.findUnique({
+  //     where: { id: todoId },
+  //   });
 
-    if (!todo) {
-      return NextResponse.json({ error: "Todo not found" }, { status: 404 });
-    }
+  //   if (!todo) {
+  //     return NextResponse.json({ error: "Todo not found" }, { status: 404 });
+  //   }
 
-    if (todo.userId !== userId) {
-      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
-    }
+  //   if (todo.userId !== userId) {
+  //     return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+  //   }
 
-    await prisma.todo.delete({
-      where: { id: todoId },
-    });
+  //   await prisma.todo.delete({
+  //     where: { id: todoId },
+  //   });
 
-    return NextResponse.json({ message: "Todo deleted successfully" });
-  } catch (error) {
-    return NextResponse.json(
-      { error: `Internal Server Error,${error}` },
-      { status: 500 }
-    );
-  }
+  //   return NextResponse.json({ message: "Todo deleted successfully" });
+  // } catch (error) {
+  //   return NextResponse.json(
+  //     { error: `Internal Server Error,${error}` },
+  //     { status: 500 }
+  //   );
+  // }
 }
